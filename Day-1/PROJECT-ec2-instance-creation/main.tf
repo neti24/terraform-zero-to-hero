9d54d2e27c9ebe415204bd4b490b565a5cfea607
neti24 <104979924+neti24@users.noreply.github.com>
2026-08-08 12:19:58 +0530

    Update main.tf

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -4,5 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "example" {
     ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+    instance_type = "t2.micro"               # Specify the type of instance you wanna use
+    subnet_id = "subnet-09deftvbiklnknhbh",  # Instances need a VPC/subnet 
+    key_name = "aws_login"                   # Required for login in to EC2 machines  
 }
